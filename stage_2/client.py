321d7dc03bca9d536350151cf7d862e16e0ea390
kenta_mitsui <66325840+kentamitsui@users.noreply.github.com>
2024-02-23 00:59:15 +0900

    next set to custom TCP protocol, header, body etc...

--- stage_2/client.py
+++ stage_2/client.py
@@ -1,37 +1,50 @@
-import socket,sys,os
+import socket
 
-def protocol_header(filename_length, json_length, data_length):
-    return filename_length.to_bytes(1, "big") + json_length.to_bytes(3, "big") + data_length.to_bytes(4, "big")
-###############################################################
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = input("type in the server's address to connect to: ")
 server_port = 9001
 
-print(f"connecting to {server_address} {server_port}".format(server_address, server_port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_address, server_port))
 
 try:
-    filepath = input("type in a file to upload: ")
-
-    with open(filepath, "rb") as file:
-        file.seek(0, os.SEEK_END)
-        filesize = file.tell()
-        file.seek(0, 0)
-
-        if filesize > pow(2, 32):
-            raise Exception("file size must be below 2GB.")
-
-        filename = os.path.basename(file.name)
-        filename_bits = filename.encode("utf-8")
-        header = protocol_header(len(filename_bits), 0, filesize)
-        client_socket.send(header)
-        client_socket.send(filename_bits)
-        data = file.read(4096)
-
-        while data:
-            print("sending...")
-            client_socket.send(data)
-            data = file.read(4096)
-        
+    room_name = input(f"enter chat room name to join or create: ")
+    client_socket.send(room_name.encode("utf-8"))
 finally:
     print("closing socket")
     client_socket.close()
+
+# def protocol_header(filename_length, json_length, data_length):
+#     return filename_length.to_bytes(1, "big") + json_length.to_bytes(3, "big") + data_length.to_bytes(4, "big")
+# ###############################################################
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_address = input("type in the server's address to connect to: ")
+# server_port = 9001
+
+# print(f"connecting to {server_address} {server_port}".format(server_address, server_port))
+
+# try:
+#     filepath = input("type in a file to upload: ")
+
+#     with open(filepath, "rb") as file:
+#         file.seek(0, os.SEEK_END)
+#         filesize = file.tell()
+#         file.seek(0, 0)
+
+#         if filesize > pow(2, 32):
+#             raise Exception("file size must be below 2GB.")
+
+#         filename = os.path.basename(file.name)
+#         filename_bits = filename.encode("utf-8")
+#         header = protocol_header(len(filename_bits), 0, filesize)
+#         client_socket.send(header)
+#         client_socket.send(filename_bits)
+#         data = file.read(4096)
+
+#         while data:
+#             print("sending...")
+#             client_socket.send(data)
+#             data = file.read(4096)
+        
+# finally:
+#     print("closing socket")
+#     client_socket.close()
--- stage_2/server.py
+++ stage_2/server.py
@@ -1,52 +1,45 @@
-import socket,os
-from pathlib import Path
+import socket,os,threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = "0.0.0.0"
 server_port = 9001
 
-file_receive = "temp"
-if not os.path.exists(file_receive):
-    os.makedirs(file_receive)
+chat_room = {}
 
-print(f"starting up on {server_address} port: {server_port}")
-server_socket.bind((server_address, server_port))
-server_socket.listen(1)
-
-while True:
-    connection, client_address = server_socket.accept()
-    
+def handle_client(connection, client_address, room_name):
     try:
-        print(f"connection from: ", client_address)
-        header = connection.recv(8)
-
-        filename_length = int.from_bytes(header[:1], "big")
-        json_length = int.from_bytes(header[1:3], "big")
-        data_length = int.from_bytes(header[4:8], "big")
-        stream_rate = 4096
-
-        print(f"received header from client.\nbyte length: title length: {filename_length}, JSON length: {json_length}, data length: {data_length}".format(filename_length, json_length, data_length))
-
-        filename = connection.recv(filename_length).decode("utf-8")
-        print(f"filename: {filename}".format(filename))
-
-        if json_length != 0:
-            raise Exception("JSON data is not currently supported.")
-        if data_length == 0:
-            raise Exception("no data to read from client.")
-        
-        with open(os.path.join(file_receive, filename), "wb+") as file:
-            while data_length > 0:
-                data = connection.recv(data_length if data_length <= stream_rate else stream_rate)
-                file.write(data)
-                print(f"received {data} bytes".format(len(data)))
-                data_length -= len(data)
-                print(data_length)
-            print("finished downloading the file from client.")
-                
-    except Exception as e:
-        print(f"error: " + str(e))
-
+        while True:
+            message = connection.recv(1024)
+            if message:
+                print(f"message from {client_address}: {message.decode('utf-8')}")
+            else:
+                break
     finally:
-        print("closing current connection")
         connection.close()
+
+def client_thread(connection, client_address):
+    try:
+        room_name = connection.recv(1024).decode("utf-8")
+        if room_name not in chat_room:
+            chat_room[room_name] = []
+        chat_room[room_name].append((connection, client_address))
+        print(f"{client_address} joined room: {room_name}")
+
+        handle_client(connection, client_address, room_name)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        connection.close()
+
+def start_server():
+    print(f"starting up on {server_address} port: {server_port}")
+    server_socket.bind((server_address, server_port))
+    server_socket.listen()
+
+    while True:
+        connection, client_address = server_socket.accept()
+        print(f"connection from: {client_address}")
+        threading.Thread(target=client_thread, args=(connection, client_address)).start()
+
+if __name__ == "__main__":
+    start_server()
