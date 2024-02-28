453daf572542d87b99aec03d2fdd903d763fa8e3
kenta_mitsui <66325840+kentamitsui@users.noreply.github.com>
2024-02-28 13:57:19 +0900

    fix response client data is send to client

--- stage_1/server.py
+++ stage_1/server.py
@@ -13,22 +13,22 @@ while True:
         data, address = server_socket.recvfrom(4096)
 
         length_username = data[0]
+        timestamp = int.from_bytes(data[1:5], byteorder="big")
         # decode username and message send from client side
         # client =>> server
+        username = data[5:length_username + 5].decode("utf-8")
+        message = data[length_username + 5:].decode("utf-8")
         
-        # username = data[1:length_username + 1].decode("utf-8")
-        # sentence = data[length_username + 1:].decode("utf-8")
-        
+        current_time = int(time.time())
+        clients[address] = {"last_active time": current_time, "errors": 0}
         # send format sentence to client side
         # server ==>> client
-        # formatted_message = f"\nmessage: {sentence} from: {username}\naddress: {address}".encode("utf-8")
+        formatted_message = f"\nmessage: {message} from: {username}\naddress: {address}".encode("utf-8")
+        print(formatted_message.decode("utf-8"))
 
-        timestamp = int.from_bytes(data[1:5], byteorder="big")
-        current_time = int(time.time())
-        clients[address] = {"last_active time: ": current_time, "errors: ": 0}
-
-        # print(f"\nmessage: {sentence} from: {username}\naddress: {address}")
-        # print(data[1:].decode("utf-8"))
+        if client_address not in clients:
+            clients[client_address] = True
+            print(f"New client connected: {client_address}")
         
         for client_address in clients.keys():
             try:
@@ -39,13 +39,10 @@ while True:
                 print(f"error sending to {client_address}: {error}")                
 
         for client_address, info in list(clients.items()):
-            if (current_time - info["last_active time: "] > 60) or (info["errors: "] >= 5):
+            if (current_time - info["last_active time"] > 60) or (info["errors"] >= 5):
                 print(f"removing client {client_address}")
                 del clients[client_address]
         
-        if client_address not in clients:
-            clients[client_address] = True
-            print(f"New client connected: {client_address}")
 
     except Exception as e:
         print(f"error: {e}")
